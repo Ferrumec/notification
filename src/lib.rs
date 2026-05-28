f68a41168382a4d7e62b2c0a2cd7047e41b99964
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-05-28 20:42:37 +0300

    removed prefs

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,13 @@
 use actix_web::web;
 use actix_web::web::ServiceConfig;
-use actixutils::{Identity, OrphanWrapper, Validate};
+use actixutils::{Identity, Validate};
 use emailgrid::EmailingContext;
-use event_stream::{EventStream, Handler};
+use event_stream::EventStream;
 use push::Config;
 use serde::{Deserialize, Serialize};
 
 use sqlx::{Pool, Sqlite};
 use std::sync::Arc;
-mod prefs;
 use mgk::{Module as Mgk, Sender};
 
 struct Push(Config);
@@ -17,14 +16,14 @@ struct Email(EmailingContext);
 
 #[async_trait::async_trait]
 impl Sender for Push {
-    async fn send(&self, address: String, subject: String, message: String) {
+    async fn send(&self, _address: String, _subject: String, message: String) {
         self.0.push("push".to_string(), message);
     }
 }
 
 #[async_trait::async_trait]
 impl Sender for Email {
-    async fn send(&self, address: String, subject: String, message: String) {
+    async fn send(&self, _address: String, subject: String, message: String) {
         self.0.send(subject, message).await;
     }
 }
@@ -33,18 +32,15 @@ impl Sender for Email {
 pub struct Module {
     emailer: Mgk,
     push_mgk: Mgk,
-    state: Arc<AppState>,
     push_: Config,
 }
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
 struct OnNotification {
-    state: Arc<AppState>,
     emailer: EmailingContext,
     push: Arc<Config>,
 }
 
-use crate::prefs::{AppState, Channel};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Event {
@@ -68,23 +64,20 @@ impl Module {
         let console = Mgk::new(pool.clone(), es.clone()).await;
         let push_ = Config::new(es, validator).await;
         let push_mgk = console.clone().with_sender(Arc::new(Push(push_.clone())));
-        let state = Arc::new(AppState::new(pool.clone()));
         let email = console
             .clone()
             .with_sender(Arc::new(Email(emailer)) as Arc<dyn Sender>);
-        let module = Self {
+        
+        Self {
             emailer: email,
-            state: state.clone(),
             push_mgk,
             push_,
-        };
-        module
+        }
     }
 
     pub fn config(&self, cfg: &mut ServiceConfig, namespace: &str) {
         cfg.service(
             web::scope(namespace)
-                .app_data(web::Data::from(self.state.clone()))
                 .configure(|cfg| self.push_.config(cfg, "/push"))
                 .configure(|cfg| self.emailer.config(cfg, "/email"))
                 .configure(|cfg| self.push_mgk.config(cfg, "/push")),
--- src/prefs/mod.rs
+++
@@ -1,7 +0,0 @@
-mod channel;
-pub mod db;
-mod handlers;
-mod routes;
-pub use channel::Channel;
-pub use routes::config;
-pub use handlers::AppState;
