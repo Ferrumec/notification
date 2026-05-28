3a53776e2838f69526905b821a273a48354f7173
Austin-rgb <prof.odhiambo.ostine@gmail.com>
2026-05-28 20:35:31 +0300

    introduced mgk

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,40 @@
 use actix_web::web;
 use actix_web::web::ServiceConfig;
 use actixutils::{Identity, OrphanWrapper, Validate};
-use async_trait::async_trait;
 use emailgrid::EmailingContext;
 use event_stream::{EventStream, Handler};
 use push::Config;
 use serde::{Deserialize, Serialize};
-use serde_json::from_str;
+
 use sqlx::{Pool, Sqlite};
 use std::sync::Arc;
 mod prefs;
+use mgk::{Module as Mgk, Sender};
+
+struct Push(Config);
+
+struct Email(EmailingContext);
+
+#[async_trait::async_trait]
+impl Sender for Push {
+    async fn send(&self, address: String, subject: String, message: String) {
+        self.0.push("push".to_string(), message);
+    }
+}
+
+#[async_trait::async_trait]
+impl Sender for Email {
+    async fn send(&self, address: String, subject: String, message: String) {
+        self.0.send(subject, message).await;
+    }
+}
 
 #[derive(Clone)]
 pub struct Module {
-    emailer: EmailingContext,
-    push: Arc<Config>,
+    emailer: Mgk,
+    push_mgk: Mgk,
     state: Arc<AppState>,
+    push_: Config,
 }
 use chrono::{DateTime, Utc};
 use uuid::Uuid;
@@ -24,7 +43,7 @@ struct OnNotification {
     emailer: EmailingContext,
     push: Arc<Config>,
 }
-use crate::prefs::config;
+
 use crate::prefs::{AppState, Channel};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -39,54 +58,26 @@ pub struct Event {
     pub session_id: Option<Uuid>,
 }
 
-#[async_trait]
-impl Handler for OnNotification {
-    async fn handle(&self, subject: String, message: Vec<u8>) {
-        let message = String::from_utf8(message).unwrap();
-        let event: Event = from_str(&message).unwrap();
-        let channel = match self
-            .state
-            .preferences
-            .get(&event.user_id.unwrap().to_string(), &subject)
-            .await
-        {
-            Ok(r) => r.unwrap(),
-            Err(e) => {
-                eprintln!("Error in reading preferences: {e}");
-                return ();
-            }
-        };
-        match channel {
-            Channel::Email => {
-                match self.emailer.send(subject, message).await {
-                    Ok(_) => (),
-                    Err(e) => eprintln!("Error in sending email: {}", e),
-                };
-            }
-            Channel::Push => {
-                self.push.push(event.producer, message);
-            }
-            Channel::Sms => (),
-            Channel::InApp => (),
-        }
-    }
-}
-
 impl Module {
     pub async fn new(
         pool: Pool<Sqlite>,
         emailer: EmailingContext,
-        validator: OrphanWrapper<Arc<dyn Validate<Identity>>>,
-        es: event_stream::OrphanWrapper<Arc<dyn EventStream>>,
+        validator: Arc<dyn Validate<Identity>>,
+        es: Arc<dyn EventStream>,
     ) -> Self {
-        let push = Arc::new(Config::new(validator.0));
+        let console = Mgk::new(pool.clone(), es.clone()).await;
+        let push_ = Config::new(es, validator).await;
+        let push_mgk = console.clone().with_sender(Arc::new(Push(push_.clone())));
         let state = Arc::new(AppState::new(pool.clone()));
+        let email = console
+            .clone()
+            .with_sender(Arc::new(Email(emailer)) as Arc<dyn Sender>);
         let module = Self {
-            emailer,
+            emailer: email,
             state: state.clone(),
-            push,
+            push_mgk,
+            push_,
         };
-        module.subscribe(es.0, state).await;
         module
     }
 
@@ -94,25 +85,9 @@ impl Module {
         cfg.service(
             web::scope(namespace)
                 .app_data(web::Data::from(self.state.clone()))
-                .configure(|cfg| self.push.config(cfg, "/push"))
-                .configure(config),
+                .configure(|cfg| self.push_.config(cfg, "/push"))
+                .configure(|cfg| self.emailer.config(cfg, "/email"))
+                .configure(|cfg| self.push_mgk.config(cfg, "/push")),
         );
-    }
-    pub async fn subscribe(&self, es: Arc<dyn EventStream>, state: Arc<AppState>) {
-        match es
-            .clone()
-            .subscribe(
-                "notification".to_string(),
-                Arc::new(OnNotification {
-                    push: self.push.clone(),
-                    emailer: self.emailer.clone(),
-                    state,
-                }),
-            )
-            .await
-        {
-            Ok(_) => (),
-            Err(e) => eprintln!("Error in subscribing to event stream: {e}"),
-        };
     }
 }
